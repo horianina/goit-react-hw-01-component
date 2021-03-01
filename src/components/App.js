4c5014df7a04de0e554a3730414a3d2e4ff1254e
Anna Horianina <anna.goryanina88@gmail.com>
2021-03-01 10:06:09 +0300

    added formating

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import user from '../data/user.json'
-import Profile from './Profile'
-import statisticalData from '../data/statistical-data.json'
-import StatisticsList from './StatisticsList'
-import friends from '../data/friends.json'
-import FriendList from './FriendList'
-import TransactionHistory from './TransactionHistory'
-import transactions from '../data/transactions.json'
+import React from 'react';
+import user from '../data/user.json';
+import Profile from './Profile';
+import statisticalData from '../data/statistical-data.json';
+import StatisticsList from './StatisticsList';
+import friends from '../data/friends.json';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
+import transactions from '../data/transactions.json';
 
-
-const App=()=>{
-  
-  return(
+const App = () => {
+  return (
     <>
-    <Profile
-  name={user.name}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  statsFollowers={user.stats.followers}
-  statsViews={user.stats.views}
-  statsLikes={user.stats.likes}/>
-    
-  <StatisticsList 
-  title="Upload stats" stats={statisticalData}  
-  StatisticsList statisticalData={statisticalData} />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        statsFollowers={user.stats.followers}
+        statsViews={user.stats.views}
+        statsLikes={user.stats.likes}
+      />
 
-<TransactionHistory transactions={transactions} />
+      <StatisticsList
+        title="Upload stats"
+        stats={statisticalData}
+        StatisticsList
+        statisticalData={statisticalData}
+      />
 
-<FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
 
+      <FriendList friends={friends} />
+    </>
+  );
+};
 
-</>
-  )
-}
-  
-
-export default App
+export default App;
